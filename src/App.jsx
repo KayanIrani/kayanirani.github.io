23f2615e0698e6b3f04180461c319fb1550a905c
KayanIrani <iranikayan5@gmail.com>
2026-09-13 18:28:52 +0530

    Completed Loading the model

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Desk from './Desk/Desk.jsx'
-
+import "./App.css"
 function App() {
   const [count, setCount] = useState(0)
 
--- src/Desk/Desk.jsx
+++ src/Desk/Desk.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 
+import Room from "./components/Room.jsx"
+
+import { CameraControls } from '@react-three/drei'
+
+const Scene = ({controls})=>{
+  // useFrame(()=>{
+  //   // console.log("Position:")
+  //   // console.log(controls.current.getPosition())
+  //   // console.log("Rotation")
+  //   // console.log(controls.current.camera.rotation)
+  // })
+}
 const Desk = () => {
+  const controls = useRef()
   return (
     <>
-        <Canvas>
-          <mesh>
+        <Canvas
+          camera={{
+              position:[-299.81,39.52,14.47],
+              fov: 70,
+            }}
+        >
+          <CameraControls ref={controls}/>
+          {/* <mesh>
             <boxGeometry />
             <meshStandardMaterial color={'red'}/>
-          </mesh>
-          <ambientLight intensity={1} />
+          </mesh> */}
+          <ambientLight intensity={5} />
+          <Scene controls={controls}/>
+          <Room />
         </Canvas>
     </>
   )
